b167f257c4da08f7bb49b573e6ae8bfaa027d8b0
nicholas278@gmail.com <nicholas278@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-04-25 10:19:21 +0000

    Changed config base_url back to NULL

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'localhost:8080/SportApp';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
