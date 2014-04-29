2d5de4b626cea33d265d5f5f5d25091010bbfc7a
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-04-29 18:11:07 +0000

    moved footer, maybe really bad

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,14 @@
-        <br><br>
-        <strong>&copy; Nicholas&Canon 2014</strong>
+	<div id="footer">
+            <strong>&copy; Nicholas&Canon 2014</strong>
+		<div ID="footerchild">
+			<a href="about.html">About</a>
+			|
+			<a href="contribute.html">Contribute</a>
+			|
+			<a href="contact.html">Contact Us</a>
+			|
+			<a href="terms.html">Terms of Use</a>
+		</div>
+	</div>
     </body>
 </html>
