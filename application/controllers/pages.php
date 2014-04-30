2b3ba73a75e13dc44b3bc44645fbc96a0dd95ebf
nicholas278@gmail.com <nicholas278@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-04-30 16:52:29 +0000

    Added some DB functions

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -5,19 +5,19 @@ class Pages extends CI_Controller {
         public function __construct()
             {
 		parent::__construct();
+                $this->load->model('sports_model');
             }
             
 	public function view($page = 'home')
 	{     
+            $data['sports_item'] = $this->sports_model->get_sports();
             if ( ! file_exists('application/views/pages/'.$page.'.php'))
             {
                 // Whoops, we don't have a page for that!
                 show_404();
             }
                 
-            $data['title'] = ucfirst($page); // Capitalize the first letter
-            $data['base']  = $this->config->item('base_url');
-            $data['css']   = $this->config->item('css');   
+            $data['title'] = ucfirst($page); // Capitalize the first letter 
             
             $this->load->view('templates/header', $data);
             $this->load->view('pages/'.$page, $data);
