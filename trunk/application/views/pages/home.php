8fab7e9c8fff6916f3dfddb9310c72f486872889
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-15 07:56:21 +0000

    alphabeticalized the list

--- trunk/application/views/pages/home.php
+++ trunk/application/views/pages/home.php
@@ -8,20 +8,20 @@
 				</h2>
 			</div>
 			<ul>    
-                                <li><a href="#" id='basketball'>Basketball</a></li>
-                                <li><a href="#" id='tennis'>Tennis</a></li>
-                                <li><a href="#" id='hockey'>Hockey</a></li>
-                                <li><a href="#" id='volleyball'>Volleyball</a></li>
-                                <li><a href="#" id='ultimate'>Ultimate</a></li>
-                                <li><a href="#" id='soccer'>Soccer</a></li>
-                                <li><a href="#" id='gym'>Gym</a></li>
-                                <li><a href="#" id='baseball'>Baseball</a></li>
                                 <li><a href="#" id='badminton'>Badminton</a></li>
-                                <li><a href="#" id='table_tennis'>Table Tennis</a></li>
-                                <li><a href="#" id='swimming'>Swimming</a></li>
-                                <li><a href="#" id='squash'>Squash</a></li>
+                                <li><a href="#" id='baseball'>Baseball</a></li>
+                                <li><a href="#" id='basketball'>Basketball</a></li>
                                 <li><a href="#" id='golf'>Golf</a></li>
+                                <li><a href="#" id='gym'>Gym</a></li>
+                                <li><a href="#" id='hockey'>Hockey</a></li>
                                 <li><a href="#" id='rock_climbing'>Rock Climbing</a></li>
+                                <li><a href="#" id='soccer'>Soccer</a></li>
+                                <li><a href="#" id='squash'>Squash</a></li>
+                                <li><a href="#" id='swimming'>Swimming</a></li>
+                                <li><a href="#" id='table_tennis'>Table Tennis</a></li>
+                                <li><a href="#" id='tennis'>Tennis</a></li>
+                                <li><a href="#" id='ultimate'>Ultimate</a></li>
+                                <li><a href="#" id='volleyball'>Volleyball</a></li>
 
 			</ul>
 		</div>
