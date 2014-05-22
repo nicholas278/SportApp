7cae0be76dfddecfbc475176dad171d4712c3170
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-22 18:35:41 +0000

    added about and contact page; needs relinking on the bottom

---
+++ trunk/application/views/pages/about.php
@@ -0,0 +1,36 @@
+	<div id="top" class="color1">
+		<div id="topwrapper">
+			<div id="logo">
+			</div>
+			<div id="topinputbox">
+				<div id="topinputcontainer">
+					<form name="input"  action="javascript:void(0);" id="searchform"> <!--nick add something for the action and method attribute, not sure what they do-->
+							<input type="text" name="user_location_input" placeholder="&nbsp;Sports, Locations, Names..." id="searchinput"><!--
+							--><input type="submit" value="Search" id="searchsubmit">			        
+					</form>
+				</div>
+			</div>
+			<div id="buttonbox">
+				<button type="button" class="color1">Home</button>
+				<button type="button" class="color1">About</button>
+				<button type="button" class="color1">Contribute</button>
+				<button type="button" class="color1">Contact Us</button>
+			</div>
+		</div>
+        </div>
+	<div id="contentwrap">
+		<div class="otherpages" class="color3">
+			<div class="pagepurposewrap">
+				<h1>
+				ABOUT
+				</h1>
+			</div>
+			<div class="pagecontentwrap">
+				<div class="pagecontent">
+					content content content content content
+					<br>content content content content content
+					<br>content content content content content
+				</div>				
+			</div>
+		</div>
+	</div>
---
+++ trunk/application/views/pages/contact.php
@@ -0,0 +1,48 @@
+	<div id="top" class="color1">
+		<div id="topwrapper">
+			<div id="logo">
+			</div>
+			<div id="topinputbox">
+				<div id="topinputcontainer">
+					<form name="input"  action="javascript:void(0);" id="searchform"> <!--nick add something for the action and method attribute, not sure what they do-->
+							<input type="text" name="user_location_input" placeholder="&nbsp;Sports, Locations, Names..." id="searchinput"><!--
+							--><input type="submit" value="Search" id="searchsubmit">			        
+					</form>
+				</div>
+			</div>
+			<div id="buttonbox">
+				<button type="button" class="color1">Home</button>
+				<button type="button" class="color1">About</button>
+				<button type="button" class="color1">Contribute</button>
+				<button type="button" class="color1">Contact Us</button>
+			</div>
+		</div>
+        </div>
+	<div id="contentwrap">
+		<div class="otherpages" class="color3">
+			<div class="pagepurposewrap">
+				<h1>
+				CONTACT US
+				</h1>
+			</div>
+			<div class="pagecontentwrap">
+				<div class="pagecontent">
+					<div class="pagecontentleft">
+					content content content content content
+					<br>content content content content content
+					<br>content content content content content
+					</div>
+					<div class="pagecontentright">
+					<form action="contact.php" method="post">
+						Your name <input type="text" name="cf_name"><br>
+						Your e-mail <input type="text" name="cf_email"><br>
+						Message <textarea name="cf_message"></textarea>
+						<br>
+						<input type="submit" value="Send">
+						<input type="reset" value="Clear">
+					</form>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
