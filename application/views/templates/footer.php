d2b074da877518c3c8c18f6a99698dd5b9ace0b9
nicholas278@gmail.com <nicholas278@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-22 19:36:05 +0000

    Added links to the other pages.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,7 @@
 	<div id="footer">
          <!--   <strong>&copy; Nicholas&Canon 2014</strong> -->
 		<div ID="footerchild">
-                        <a href="index.php">Home</a>
+                        <a href="<?php echo base_url(); ?>index.php">Home</a>
 			|
 			<a href="<?php echo base_url(); ?>index.php/about">About</a>
 			|
