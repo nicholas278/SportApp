c936802704f5aee7ef59c265eaf20cc7bd2dcea5
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-23 18:12:17 +0000

    fixed labeling in contact us of input names

--- trunk/application/views/pages/contact.php
+++ trunk/application/views/pages/contact.php
@@ -17,22 +17,22 @@
 						<div class="formlabel">
 							<label for="Name">Your Name:</label>
 						</div>
-						<input type="text" maxlength="35" class="formtextbox" name="c_name">
+						<input type="text" maxlength="35" class="formtextbox" name="cu_name">
 						<br>
 						<div class="formlabel">
 							<label for="Email">Your Email:</label>
 						</div>
-						<input type="text" maxlength="60" class="formtextbox" name="c_email">
+						<input type="text" maxlength="60" class="formtextbox" name="cu_email">
 						<br>
 						<div class="formlabel">
 							<label for="Subject">Subject:</label>
 						</div>
-						<input type="text" maxlength="60" class="formtextbox" name="c_subject"><br>
+						<input type="text" maxlength="60" class="formtextbox" name="cu_subject"><br>
 						<div class="formlabel">
 							<label for="Message">Message:</label>
 						</div>
 						<div class="messages">
-						<textarea name="cf_message"></textarea>
+						<textarea name="cu_message"></textarea>
 							<br>
 							<input type="submit" value="Send" ><input type="reset" value="Clear">
 						</div>
