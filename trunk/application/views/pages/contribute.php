41fe604cb9f74d916a3b74f66c68caa1d8f670cd
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-23 22:34:27 +0000

    contribute/terms/contact pages updated

---
+++ trunk/application/views/pages/contribute.php
@@ -0,0 +1,73 @@
+	<div id="contentwrap">
+		<div class="otherpages" class="color3">
+			<div class="pagepurposewrap">
+				<h1>
+				CONTRIBUTE
+				</h1>
+			</div>
+			<div class="pagecontentwrap">
+				<div class="pagecontent">
+					<div class="pagecontentleft">
+					content content content content content
+					<br>content content content content content
+					<br>content content content content content
+					</div>
+					<div class="pagecontentright">
+					<form action="contact.php" method="post" id="contributeform">
+						<div class="formlabel">
+							<label for="Name">Your Name:</label>
+						</div>
+						<input type="text" maxlength="35" class="formtextbox" name="c_name">
+						<br>
+						<div class="formlabel">
+							<label for="Email">Your Email:</label>
+						</div>
+						<input type="text" maxlength="60" class="formtextbox" name="c_email">
+						<br>
+						<div class="formlabel">
+							<label for="Subject">Sport:*</label>
+						</div>
+							<form action="URL"> <!--finish off the action nick-->
+								<select name="sportsinput" id="sportinput">
+									<option value="default" selected id="defaultsportinput">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Sports -</option>
+									<option value="Badminton">Badminton</option>
+									<option value="Baseball">Baseball</option>
+									<option value="Basketball">Basketball</option>
+									<option value="Golf">Golf</option>
+									<option value="Gym">Gym</option>
+									<option value="Hockey">Hockey</option>
+									<option value="RockClimbing">Rock Climbing</option>
+									<option value="Soccer">Soccer</option>
+									<option value="Squash">Squash</option>									
+									<option value="Swimming">Swimming</option>
+									<option value="TableTennis">Table Tennis</option>
+									<option value="Tennis">Tennis</option>
+									<option value="Ultimate">Ultimate</option>
+									<option value="Volleyball">Volleyball</option>
+									<option value="Yoga">Yoga</option>
+								</select>
+							</form>
+						<input type="text" maxlength="60" id="othersportsbox" class="formtextbox" name="c_othersport" placeholder="&nbsp;Other Sports">
+						<br>
+						<div class="formlabel">
+							<label for="Location">Location:*</label>
+						</div>
+						<input type="text" maxlength="60" class="formtextbox" name="c_location">
+						<br>
+						<div class="formlabel">
+							<label for="OtherInfo">Other Information:</label>
+						</div>
+						<div class="otherinfo">
+						<textarea name="c_otherinfo"></textarea>
+							<br>
+							<input type="submit" value="Send" ><input type="reset" value="Clear">
+							<p>
+							*mandatory fields
+							</p>
+						</div>
+					</form>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
---
+++ trunk/application/views/pages/terms.php
@@ -0,0 +1,16 @@
+	<div id="contentwrap">
+		<div class="otherpages" class="color3">
+			<div class="pagepurposewrap">
+				<h1>
+				TERMS OF USE
+				</h1>
+			</div>
+			<div class="pagecontentwrap">
+				<div class="pagecontent">
+					content content content content content
+					<br>content content content content content
+					<br>content content content content content
+				</div>				
+			</div>
+		</div>
+	</div>
