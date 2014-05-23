f651902b7fe245e33447f1040f08445f82d31826
nicholas278@gmail.com <nicholas278@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-23 01:28:50 +0000

    Added links to the menu.

--- trunk/application/views/pages/home.php
+++ trunk/application/views/pages/home.php
@@ -1,24 +1,4 @@
 <!--HTML body goes here-->
-	<div id="top" class="color1">
-		<div id="topwrapper">
-			<div id="logo">
-			</div>
-			<div id="topinputbox">
-				<div id="topinputcontainer">
-					<form name="input"  action="javascript:void(0);" id="searchform">
-							<input type="text" name="user_location_input" placeholder="&nbsp;Sports, Locations, Names..." id="searchinput"><!--
-							--><input type="submit" value="Search" id="searchsubmit">			        
-					</form>
-				</div>
-			</div>
-			<div id="buttonbox">
-				<button type="button" class="color1">Home</button>
-                                <button type="button" class="color1">About</button>
-                                <button type="button" class="color1">Contribute</button>
-                                <button type="button" class="color1">Contact Us</button>
-			</div>
-		</div>
-        </div>
 	<div id="contentwrap">
 		<div id="leftbar" class="color6">
 			<div class="leftbarheader">
--- trunk/application/views/templates/footer.php
+++ trunk/application/views/templates/footer.php
@@ -3,13 +3,13 @@
 		<div ID="footerchild">
                         <a href="<?php echo base_url(); ?>index.php">Home</a>
 			|
-			<a href="<?php echo base_url(); ?>index.php/about">About</a>
+			<a href="<?php echo base_url(); ?>index.php/view/about">About</a>
 			|
-			<a href="<?php echo base_url(); ?>index.php/contribute">Contribute</a>
+			<a href="<?php echo base_url(); ?>index.php/view/contribute">Contribute</a>
 			|
-			<a href="<?php echo base_url(); ?>index.php/contact">Contact Us</a>
+			<a href="<?php echo base_url(); ?>index.php/view/contact">Contact Us</a>
 			|
-			<a href="<?php echo base_url(); ?>index.php/terms">Terms of Use</a>
+			<a href="<?php echo base_url(); ?>index.php/view/terms">Terms of Use</a>
 		</div>
 	</div>
     </body>
