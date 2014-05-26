ef17d4a47eb5538c976b577b673939900447c232
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-26 05:27:42 +0000

    pages updated with content (except front page)

--- trunk/application/views/pages/terms.php
+++ trunk/application/views/pages/terms.php
@@ -7,9 +7,10 @@
 			</div>
 			<div class="pagecontentwrap">
 				<div class="pagecontent">
-					content content content content content
-					<br>content content content content content
-					<br>content content content content content
+                                    [Insert Name] works to provide you with information on active living in your area. Much of our content comes from our community, user-generated, who keeps us up to date with the newest developments. Contributions by users may be subject to review by [Insert Name] before they are made visible to all users.
+                                    <br>
+                                    <br>
+                                    Due to the source of our information, [Insert Name] makes no representations or warranties regarding the accuracy or completeness of any content or products on this website. [Insert Name] will not be liable for any damage or loss resulting from your use of the content or the products.
 				</div>				
 			</div>
 		</div>
