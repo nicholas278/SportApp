705e123776328a412719fc9029d07b0add1315ec
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-26 21:10:29 +0000

    updated terms content link .4

--- trunk/application/views/pages/terms.php
+++ trunk/application/views/pages/terms.php
@@ -2,7 +2,7 @@
 		<div class="otherpages" class="color3">
 			<div class="pagepurposewrap">
 				<h1>
-				Terms of Service
+				TERMS OF SERVICE
 				</h1>
 			</div>
 			<div class="pagecontentwrap">
