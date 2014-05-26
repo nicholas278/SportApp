3c728733475c1ccc5b7eae7e364a0f31fc6cee08
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-26 21:08:32 +0000

    updated terms content

--- application/views/pages/terms.php
+++ application/views/pages/terms.php
@@ -2,15 +2,22 @@
 		<div class="otherpages" class="color3">
 			<div class="pagepurposewrap">
 				<h1>
-				TERMS OF USE
+				TERMS OF Service
 				</h1>
 			</div>
 			<div class="pagecontentwrap">
 				<div class="pagecontent">
                                     <ol>
-                                        <li>[Insert Name] works to provide you with information on active living in your area. Much of our content comes from our community, user-generated, who keeps us up to date with the newest developments. Contributions by users may be subject to review by [Insert Name] before they are made visible to all users.</li>
+                                        <li>Terms of Use: [Insert Name] works to provide you with information on active living in your area. Much of our content comes from our community, user-generated, who keeps us up to date with the newest developments. [Insert Name] is for personal, non-commercial, use only. Use of [Insert Name] includes both accessing content and contributing content.</li>
                                         <br>
-                                        <li>Due to the source of our information, [Insert Name] makes no representations or warranties regarding the accuracy or completeness of any content or products on this website. [Insert Name] will not be liable for any damage or loss resulting from your use of the content or the products.</li>
+                                        <li>Our Content: We are the sole and exclusive owners of “Our Content”, including, but not limited to, visual interfaces, interactive features, graphics, design, compilation, computer code, products, software, aggregate user review ratings, reports and other usage-related data in connection with activities the use of the [Insert Name] website.</li>
+                                        <br>
+                                        <li>Restriction of Use: Without limiting the generality of these Terms, you specifically agree not to do the following while using [Insert Name] to modify, copy, publish, license, sell, rent, lease, lend, transfer or otherwise commercialize any rights to or Our Content;</li>
+                                        <br>
+                                        <li>Contribution: By submitting information or content through the contribute form, or the contact form, you grant us a royalty-free, perpetual, irrevocable, non-exclusive, unrestricted, worldwide licence to reproduce, prepare derivative works, distribute copies, perform, or publicly display your content in any medium and for any purpose including commercial purposes, and to authorize others to do so. Contributions by users may be subject to review by [Insert Name] before they are made visible to all users.</li>
+                                        <br>
+                                        <li>Disclaimer of Warranties and Limitation of Liability: Due to the source of our information, [Insert Name] makes no representations or warranties regarding the accuracy or completeness of any content or products on this website. [Insert Name] will not be liable for any damage or loss resulting from your use of the content or the products.</li>
+                                        <br>
                                     </ol>
                                 </div>				
 			</div>
