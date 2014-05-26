49405b817e65a8edd2a8f53b8074f82e785c9785
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-26 21:09:50 +0000

    updated terms content link .2

--- application/views/pages/terms.php
+++ application/views/pages/terms.php
@@ -12,7 +12,7 @@
                                         <br>
                                         <li>Our Content: We are the sole and exclusive owners of “Our Content”, including, but not limited to, visual interfaces, interactive features, graphics, design, compilation, computer code, products, software, aggregate user review ratings, reports and other usage-related data in connection with activities the use of the [Insert Name] website.</li>
                                         <br>
-                                        <li>Restriction of Use: Without limiting the generality of these Terms, you specifically agree not to do the following while using [Insert Name] to modify, copy, publish, license, sell, rent, lease, lend, transfer or otherwise commercialize any rights to or Our Content;</li>
+                                        <li>Restriction of Use: Without limiting the generality of these Terms, you specifically agree not to do the following while using [Insert Name] to modify, copy, publish, license, sell, rent, lease, lend, transfer or otherwise commercialize any rights to or Our Content.</li>
                                         <br>
                                         <li>Contribution: By submitting information or content through the contribute form, or the contact form, you grant us a royalty-free, perpetual, irrevocable, non-exclusive, unrestricted, worldwide licence to reproduce, prepare derivative works, distribute copies, perform, or publicly display your content in any medium and for any purpose including commercial purposes, and to authorize others to do so. Contributions by users may be subject to review by [Insert Name] before they are made visible to all users.</li>
                                         <br>
