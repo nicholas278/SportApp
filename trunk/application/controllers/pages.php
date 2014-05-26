f453dfdf5aec7082d78cb88703f173b61de80b10
nicholas278@gmail.com <nicholas278@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-05-26 01:13:16 +0000

    Commited for campbell river.  Code not not error free.

--- trunk/application/controllers/pages.php
+++ trunk/application/controllers/pages.php
@@ -1,12 +1,14 @@
 <?php
 
 class Pages extends CI_Controller {
-
+        private $filterArray, $reservedArray = [];
+    
         public function __construct()
             {
 		parent::__construct();
                 $this->load->model('sports_model');
                 $this->load->helper('url');
+                $filterArray = [];
             }
             
         public function view($page = 'home'){                      
@@ -37,4 +39,43 @@ class Pages extends CI_Controller {
             
             $this->load->view('templates/results', $data);
 	}
+        
+        public function add_filter($filter){
+            //Get input
+            $filterType = $this->input->post('');
+            $filterValue = $this->input->post('');
+            //check what filter it is being applied
+            if($filterType === "sport"){
+                //check if filter is already been applied
+                for($i=0; $i<sizeof($this->filterArray); $i++){
+                    if($this->filterArray[$i].key == "sport"){
+                        //if applied, check what position the filter is applied
+                        if($i==0){
+                            //if it is the 1st filter, search DB
+                            $data['sports'] = $this->sports_model->get_sports_byType($filterValue);
+                            //then filter new list with the rest of filters
+                        }
+                        else{
+                            //remove filter, then reapply with new filter
+                        }
+                    }
+                }
+                $this->filterArray["sport"] = $filterValue;
+                $data['sports'] = $this->sports_model->get_sports_byType($filterValue);
+            }
+            elseif($filterType == "location"){
+                $this->filterArray["location"] = $filterValue;
+                $data['sports'] = $this->sports_model->get_sports_byLoc($filterValue);
+            }
+            $this->load->view('templates/results', $data);
+        }
+        
+        private function filter($filterArray, $filterList){
+            //pop filtered elements from filterArray into reservedArray
+        }
+        
+        private function unfilter($filterArray, $filterList){
+            //filter reserve array for the current filters and add to the filterArray
+        }
+        
 }
