111362dcda7aeb05833f3eeb7d93f099ea3693d6
canon31@gmail.com <canon31@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-06-01 02:29:35 +0000

    login page

---
+++ trunk/application/views/pages/admin.php
@@ -0,0 +1,27 @@
+		<div id="contentwrap">
+		<div class="otherpages" class="color3">
+			<div class="pagepurposewrap">
+				<h1>
+				ADMIN LOGIN
+				</h1>
+			</div>
+			<div class="pagecontentwrap">
+				<div class="pagecontent">
+					<div class="pagecontentmiddle">
+					<form action="contact.php" method="post" id="contactform">
+						<div class="formlabel">
+							<label for="Name">Username:</label>
+						</div>
+						<input type="text" maxlength="35" class="formtextbox" name="al_uname">
+						<br>
+						<div class="formlabel">
+							<label for="Email">Password:</label>
+						</div>
+						<input type="text" maxlength="60" class="formtextbox" name="al_password">
+						<br>
+						</div>
+					</form>
+					</div>
+				</div>				
+			</div>
+                </div>
