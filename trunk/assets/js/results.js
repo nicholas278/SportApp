17d241c215afd2c0af3b46a8dc1150886777c636
nicholas278@gmail.com <nicholas278@gmail.com@6c04c155-3232-ffe8-4d4c-5a82125a5058>
2014-06-26 19:08:22 +0000

    Bug fix: hide page navigation buttons when no results are returned

--- trunk/assets/js/results.js
+++ trunk/assets/js/results.js
@@ -7,8 +7,6 @@ function displayResults(filtersList, sportArray, currentPage, maxPage){
         createMarker(new google.maps.LatLng(sportArray[i].latitude, sportArray[i].longitude));
         createNewItem(sportArray[i].name, sportArray[i].address, sportArray[i]["sport"], sportArray[i]["distance"], i);
     }
-    var element=document.getElementById("currentpage");
-    element.innerHTML = currentPage + " / " + maxPage;
     if(markers.length > 0){
         adjustZoom();
     }
@@ -101,7 +99,13 @@ function createElement(tag, property){
 
 //Hide or show page navigation buttons when not needed
 function pageNavButtons(currentPage, maxPage){
-    if(currentPage === 1 && currentPage === maxPage){
+    var element=document.getElementById("currentpage");
+    element.innerHTML = currentPage + " / " + maxPage;
+    
+    if(currentPage > maxPage){
+        element.innerHTML = "";
+    }
+    else if(currentPage === 1 && currentPage === maxPage || currentPage > maxPage){
         document.getElementById("firstpage").firstElementChild.style.display = "none";
         document.getElementById("previouspage").firstElementChild.style.display = "none";
         document.getElementById("nextpage").firstElementChild.style.display = "none";
